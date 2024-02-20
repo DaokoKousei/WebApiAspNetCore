14d4cfed19297a0a1360009822f9dc1a86c312da
Romulo Cavalcante <32405868+DaokoKousei@users.noreply.github.com>
2024-02-20 13:38:06 -0300

    add

---
+++ WebAPI_Video/WebAPI_Video/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Video.Models;
+
+namespace WebAPI_Video.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
